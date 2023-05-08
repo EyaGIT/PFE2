fe149b35d7e4497a20c654e1e02a145bddfabdf9
EyaGIT <108862009+EyaGIT@users.noreply.github.com>
2023-05-08 20:18:08 +0100

    update

--- src/components/PickerIm/PicherIm.js
+++ src/components/PickerIm/PicherIm.js
@@ -1,10 +1,11 @@
-import { View, Text,Image,StyleSheet } from 'react-native'
+import { View, Text,Image,StyleSheet,SafeAreaView } from 'react-native'
 import React,{useState, useRef,useCallback} from 'react'
 import ImagePickerAvatar from '../uplode_Image/ImagePickerAvatar';
 import ImagePickerHeader from '../uplode_Image/ImagePickerHeader';
 import ImagePickerModal from '../uplode_Image/ImagePickerModal';
 import { launchImageLibrary } from 'react-native-image-picker';
 import * as ImagePicker from 'react-native-image-picker'
+
 
 const PicherIm = () => {
     const [pickerResponse,setPickerResponse]=useState(null);
@@ -37,7 +38,7 @@ const PicherIm = () => {
     },[]);
     const uri = pickerResponse?.assets && pickerResponse.assets[0].uri;
   return (
-    <View style={{alignItems:"center",justifyContent:"flex-start",width:300,height:150,flexGrow:1,marginTop:-150,top:140,marginBottom:170}} >
+    <SafeAreaView  >
     <ImagePickerAvatar  style={{backgroundColor:"red",margin:0,padding:0,top:-20,height:100}} uri={uri} onPress={() => setVisible(true)}/>
     <ImagePickerModal
     style={{backgroundColor:"red",height:100}}
@@ -47,7 +48,7 @@ const PicherIm = () => {
     OnCameraPress={onCameraPress}
     />
       
-    </View>
+    </SafeAreaView>
   )
 }
 
--- src/screens/EditProfil/EditProfil.js
+++ src/screens/EditProfil/EditProfil.js
@@ -1,11 +1,28 @@
-import { View, Text,ScrollView ,SafeAreaView} from 'react-native'
+import { View, Text ,ScrollView ,SafeAreaView,Image} from 'react-native'
 import React from 'react'
 import PicherIm from '../../components/PickerIm/PicherIm'
+import User from '../../../assets/images/User.png'
+import Toggle from '../../../assets/images/Toggle.png'
+
+
 
 const EditProfil = () => {
   return (
-    <SafeAreaView style={{ width:'100%',justifyContent: "flex-start", alignItems: "center"}} >
+    <SafeAreaView style={{ width:'100%',justifyContent: "flex-start", alignItems: "center",flex:1}} >
+     <View style={{alignItems:"center",justifyContent:"flex-start",width:300,height:150,flex:1,marginTop:-150,top:140,marginBottom:170}}> 
     <PicherIm/>
+    </View>  
+
+    <View style={{flex:1.5,flexDirection:'column',width:'100%',alignItems:"center"}}>
+
+        <Image source={{User}}   /> 
+         <Text style={{color:'black',fontSize:16,fontWeight:'600'}}>Edit Profile </Text> 
+         <Image source={{Toggle}}/>
+        
+        
+    </View>
+
+    
 
 
 
