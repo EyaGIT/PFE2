397c867deaba0d46b9846a7717a247b86a6a13dc
EyaGIT <108862009+EyaGIT@users.noreply.github.com>
2023-06-02 00:58:15 +0100

    update

--- src/api/user_api.js
+++ src/api/user_api.js
@@ -299,3 +299,24 @@ export const getHistory = async (page, limit, accessToken) => {
     throw new Error('Failed to fetch history data');
   }
 };
+
+export const edituser = async (token, userData) => {
+  try {
+    const result = await ApiManager('/editUser', {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}` 
+      },
+      body: JSON.stringify(userData)
+    });
+    
+    return result;
+  } catch (error) {
+    if (error.response && error.response.data) {
+      return error.response.data;
+    } else {
+      return { error: 'An error occurred.' };
+    }
+  }
+};
